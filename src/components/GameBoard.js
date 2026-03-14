07560f97bdba4895221788979106f992e0748175
mithagresiaharahap241713057-droid <mithagresiaharahap241713057@gmail.com>
2026-03-14 15:55:48 +0700

    Update GameBoard.js
    initial commit

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,20 +2,19 @@ import React from "react";
 import Card from "./Card";
 
 function GameBoard({ cards, flippedCards, matchedCards, onFlip }) {
-
-  return (
-    <div className="grid grid-cols-4 gap-4 p-4 justify-center">
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          card={card}
-          isFlipped={flippedCards.includes(card.id)}
-          isMatched={matchedCards.includes(card.id)}
-          onFlip={onFlip}
-        />
-      ))}
-    </div>
-  );
+        return (
+                <div className="grid grid-cols-4 gap-4 p-4 justify-center">
+                        {cards.map((card) => (
+                                <Card
+                                          key={card.id}
+                                          card={card}
+                                          isFlipped={flippedCards.includes(card.id)}
+                                          isMatched={matchedCards.includes(card.id)}
+                                          onFlip={onFlip}
+                                />
+                        ))}
+                </div>
+        );
 }
-
+  
 export default GameBoard;
